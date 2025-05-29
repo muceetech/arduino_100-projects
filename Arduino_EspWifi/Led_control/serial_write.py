13f79162cf358aaee40fba8473638a39be766679
muceetech <65214908+muceetech@users.noreply.github.com>
2025-05-29 23:04:12 +0530

    Update serial_write.py

--- Arduino_EspWifi/Led_control/serial_write.py
+++ Arduino_EspWifi/Led_control/serial_write.py
@@ -9,7 +9,19 @@ from time import sleep
 #  GPIO 21(tx)   10 (rx in software serial)
 #  GPIO 20(rx)   11 (tx in software serial)
 
+from machine import UART,Pin
+from time import sleep
+
 uart1 = UART(0, baudrate=9600, tx=21, rx=20)
+P0 = Pin(0, Pin.IN,Pin.PULL_UP)     # create input pin on GPIO0
+
 while True:
-    uart1.write("hello\n")  # write 5 bytes
-    sleep(1)
+    print(P0.value())
+    if P0.value() == 1:
+        uart1.write("led on\n")  
+        sleep(0.2)
+    if P0.value() == 0:
+        uart1.write("led off\n") 
+        sleep(0.2)
+    
+
